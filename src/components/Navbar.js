81e160c8289d98990a97997f2a355e706b7b0816
bulabd <bulabd@outlook.com>
2022-10-08 18:41:40 -0400

    Style the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,17 @@
+import './Navbar.css';
+
 export default function Navbar() {
   return (
-    <div>
-      <p>Navbar!!!!</p>
+    <div className='navbar'>
+      <div className='movingName'>
+        <h1 className='letterB'>B</h1><h1 className='letters'>ulat</h1><h1 className='letterA'>A</h1><h1 className='letters'>bdullin</h1>
+      </div>
+      <div className='pageLinks'>
+        <a className='anchorLink' href="#">About</a>
+        <a className='anchorLink' href="#">Experience</a>
+        <a className='anchorLink' href="#">Projects</a>
+        <a className='anchorLink' href="#">Contact</a>
+      </div>
     </div>
   );
 }
