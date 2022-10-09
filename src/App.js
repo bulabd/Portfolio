409de0059c55531d19039dbf86015f53e15f4828
bulabd <bulabd@outlook.com>
2022-10-09 12:25:37 -0400

    Create main component and apply font-family to the whole App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar.js';
+import Main from './components/Main/Main.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,9 @@
+import './Main.css';
+
+export default function Main() {
+  return (
+    <div className='main'>
+      <h1>MAIN</h1>
+    </div>
+  );
+}
