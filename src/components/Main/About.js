de158b256f73161795478270f856f041ac8b12cd
bulabd <bulabd@outlook.com>
2022-10-11 14:48:12 -0400

    Finish about page

--- src/components/Main/About.js
+++ src/components/Main/About.js
@@ -2,7 +2,8 @@ import './About.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faJs } from '@fortawesome/free-brands-svg-icons';
 import { faReact } from '@fortawesome/free-brands-svg-icons';
-
+import { faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 export default function About() {
   return (
@@ -17,8 +18,8 @@ export default function About() {
       <div className='skills'>
         <p><strong className='strong'><FontAwesomeIcon icon={faJs} /> Languages: </strong>Javascript, HTML, CSS, SQL, Ruby, Python</p>
         <p><strong className='strong'><FontAwesomeIcon icon={faReact} /> Frameworks and libraries: </strong>NodeJS, React, Express, EJS, jQuery, Axios, Rails, SASS, Bootstrap</p>
-        <p><strong className='strong'>Systems and Databases: </strong>PostgreSQL, Git</p>
-        <p><strong className='strong'> Testing: </strong>Mocha and Chai, Jest, Cypress, Storybook</p>
+        <p><strong className='strong'><FontAwesomeIcon icon={faDatabase} /> Systems and Databases: </strong>PostgreSQL, Git</p>
+        <p><strong className='strong'><FontAwesomeIcon icon={faCheck} /> Testing: </strong>Mocha and Chai, Jest, Cypress, Storybook</p>
       </div>
     </div>
   );
