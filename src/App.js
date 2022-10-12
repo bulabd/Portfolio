7c6ab06e1fdc7c74f666609a702d1cbb92291fad
bulabd <bulabd@outlook.com>
2022-10-12 11:26:28 -0400

    Add a scrollToTop button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,41 @@ import './App.css';
 import Navbar from './components/Navbar.js';
 import Main from './components/Main/Main.js';
 
+import { useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleArrowUp } from '@fortawesome/free-solid-svg-icons';
+
+
 function App() {
+
+  const [visible, setVisible] = useState(false);
+  
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 70) {
+      setVisible(true);
+    } else if (scrolled <= 70) {
+      setVisible(false);
+    }
+  };
+  
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  window.addEventListener('scroll', toggleVisible);
+
   return (
     <div className="App">
       <Navbar />
       <Main />
+      <div className='scrollToTop'>
+        <FontAwesomeIcon onClick={scrollToTop} icon={faCircleArrowUp} style={{display: visible ? 'inline' : 'none'}} />
+      </div>
     </div>
   );
 }
