0c8b1cd1f8b6fbce513c1b36c162cdd567d9bc40
bulabd <bulabd@outlook.com>
2022-10-13 19:41:38 -0400

    Add lighthouse experience

--- src/components/Main/Experience.js
+++ src/components/Main/Experience.js
@@ -4,6 +4,7 @@ export default function Experience() {
   return (
     <div id='experience' className='experience'>
       <p className='myExperience'>Experience</p>
+      <p className='work'><span className='year'>2022 - 2022: </span>Coding bootcamp at <a className='location' href='https://www.lighthouselabs.ca/' target='blank' >Lighthouse Labs</a></p>
     </div>
   );
 }
