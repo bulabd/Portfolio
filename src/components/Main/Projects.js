f17042f92d122af59b3a843d00f5ab42cac42ee9
bulabd <bulabd@outlook.com>
2022-10-15 11:53:10 -0400

    Finish styling eac and wikimaps projects

--- src/components/Main/Projects.js
+++ src/components/Main/Projects.js
@@ -1,6 +1,9 @@
 import './Projects.css';
 import ImageSlider from './ImageSlider/ImageSlider';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 export default function Projects() {
 
   const slidesEAC = [
@@ -19,13 +22,28 @@ export default function Projects() {
   return (
     <div id='projects' className='projects'>
       <p className='myProjects'>Projects</p>
-      <div className='eac'>
+      <div className='project'>
         <ImageSlider slides={slidesEAC} />
-        <p>EAC (Everyone's a Critic)</p>
+        <div className='details'>
+          <div>
+            <p className='projectTitle'>EAC (Everyone's a Critic)</p>
+            <p>A movies app where users can search for movies, review them and create their own playlists.</p>
+            <p className='stack'> &gt;&gt; JavaScript, React, NodeJS, PostgreSQL, SCSS, Material-UI, Express</p>
+            <a className='githubIcon' href='https://github.com/bulabd/final-project' target='blank'><FontAwesomeIcon icon={faGithub} /></a>
+          </div>
+        </div>
       </div>
-      <div className='wikiMaps'>
+      <span className='separator'></span>
+      <div className='project'>
         <ImageSlider slides={slidesWikiMaps} />
-        <p>WikiMaps</p>
+        <div className='details'>
+          <div>
+            <p className='projectTitle'>WikiMaps</p>
+            <p>An app where users can create custom maps, customize and share them.</p>
+            <p className='stack'> &gt;&gt; JavaScript, EJS, SCSS, NodeJS, PostgreSQL, Express</p>
+            <a className='githubIcon' href='https://github.com/bulabd/Wiki-Map' target='blank'><FontAwesomeIcon icon={faGithub} /></a>
+          </div>
+        </div>
       </div>
     </div>
   );
